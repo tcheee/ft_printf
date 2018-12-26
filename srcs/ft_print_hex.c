b44d6e9c46f4159897b387e43976c9729aeb2d89
Tom Che <B00705488@essec.edu>
2018-12-26 20:20:18 +0100

    %p

--- srcs/ft_print_hex.c
+++ srcs/ft_print_hex.c
@@ -17,14 +17,25 @@ int		ft_print_hex(t_flag flag, va_list ap)
 	void				*ptr;
 	unsigned long long	addr;
 	int					nb;
+	int					size;
 
 	nb = 0;
 	ptr = va_arg(ap, void*);
 	addr = (unsigned long long)ptr;
-	flag_space(flag, &nb);
-	ft_putstr("0x");
-	nb +=2;
-	ft_putnbr_base_addr(addr, 16, &nb);
-	flag_space_neg(flag.space, nb, &nb);
+	if (!((flag.precis == 0 || flag.precis == -5) && addr == 0))
+		ft_nblen(addr, 16, &nb);
+	nb += 2;
+	size = nb;
+	if (flag.zero == 1)
+		ft_putstr("0x");
+	if (flag.sign != 3 && flag.sign != 4)
+		flag_space(flag, &nb);
+	if (flag.zero == 0)
+		ft_putstr("0x");
+	flag_precision_nb(flag, size, &nb);
+	if (!((flag.precis == 0 || flag.precis == -5) && addr == 0))
+		ft_putnbr_base(addr, 16);
+	if (flag.space == 3 || flag.space == 4)
+		flag_space_neg(flag.space, nb, &nb);
 	return (nb);
 }
